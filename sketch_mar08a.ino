905c585fcd95b2dee2a9f37e7121d6a34c93ea6b
irak-9 <78151085+irak-9@users.noreply.github.com>
2021-03-08 01:36:53 +0530

    Add files via upload

--- sketch_mar08a.ino
+++ sketch_mar08a.ino
@@ -1,15 +1,11 @@
-#define buzzer 12  // from the circuit we can see that we have connected LED on Pin 13
-
-
+#define buzzer 12  //Pin 12
 void setup() 
 {                
-    pinMode(buzzer, OUTPUT); // Defining LED pin as OUTPUT Pin.   
+    pinMode(buzzer, OUTPUT); // Defining buzzer pin as OUTPUT Pin.   
 }
-
-// Below mentioned code runs for ever(infinite loop)
 void loop() {
-  digitalWrite(buzzer, HIGH); // LED gets turned ON (1/HIGH/+5V)
-  delay(200);             // Waiting for one second. 
-  digitalWrite(buzzer, LOW);  // LED gets OFF (0/LOW/0V/GND)
-  delay(3000);        // here and above Delay is in mili second (1000 = 1 second)
+  digitalWrite(buzzer, HIGH); // buzzer gets turned ON (1/HIGH/+5V/D12)
+  delay(200);             // Waiting for 200 milli second. 
+  digitalWrite(buzzer, LOW);  // buzzer gets OFF (0/LOW/0V/GND)
+  delay(3000);        // here and above Delay is in milli second (3000 = 3 second)
 }
